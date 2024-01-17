c07a5604654b36e5b10204e0610352b81b82da69
SAKSHI KUMARI <sakshi.kumari@appinventiv.com>
2024-01-17 11:36:32 +0000

    Update file deploy-dev.sh

--- .k8s-dev/deploy-dev.sh
+++ .k8s-dev/deploy-dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # read the yml template from a file and substitute the string 
 
-template=`cat "./.k8s-dev/deployment.yaml" | sed "s@{{DOCKER_REGISTRY}}@$DOCKER_REGISTRY_STAGE@g"`
+template=`cat "./.k8s-dev/deployment.yaml" | sed "s@{{DOCKER_REGISTRY}}@$DOCKER_REGISTRY_DEV@g"`
 
 # apply the yml with the substituted value
 echo "$template" | kubectl apply -f -
