a8ba5db7f65b24c4bf83f32a71c1e2ce7507a1ef
faisal.khan <83350838+faisal-appinventiv@users.noreply.github.com>
2024-01-29 16:02:10 +0530

    about changes

--- components/download-here/index.tsx
+++ components/download-here/index.tsx
@@ -6,16 +6,14 @@ import AppStoreIcon from "../icons/app-store";
 const DownloadHere = () => {
   return (
     <section className={styles.getStatedWrapper}>
+      <div
+        className={styles.getStatedBannerImage}
+        style={{
+          backgroundImage: `url(${"/images/checkout-app.jpg"})`,
+        }}
+      ></div>
+
       <div className={styles.getStatedContainer}>
-        <div
-          className={styles.getStatedBannerImage}
-          style={{
-            background: `url(${"/images/checkout-app.jpg"})`,
-            backgroundSize: "100%",
-            backgroundPosition: "bottom",
-            backgroundRepeat: "no-repeat",
-          }}
-        ></div>
         <div className={styles.getStatedContentWrapper}>
           <div className={styles.getStatedInfoBlock}>
             <h3 className={styles.title}>Download Today! </h3>
@@ -23,15 +21,34 @@ const DownloadHere = () => {
               Well, what are you waiting for? Download the app to experience
               Maxxx level.
             </p>
-            <div className={styles.playButton}>
-              <Button variant={"primary_outline"} className={styles.button}>
-                <AppStoreIcon />
-                App Store
-              </Button>
-              <Button variant={"primary_outline"} className={styles.button}>
-                <PlayStoreIcon />
-                Play Store
-              </Button>
+            <div className={styles.countInfoBlock}>
+              <div className="flex flex-col">
+                <div className="flex items-center">
+                  <Image
+                    className="mr-2"
+                    src={"/images/coach-icon.png"}
+                    width={24}
+                    height={24}
+                    alt="coach icon"
+                  />
+                  <div className={styles.count}>100+</div>
+                </div>
+                <div className={styles.name}>Coaches</div>
+              </div>
+              <div className={styles.borderBetween}></div>
+              <div className="flex flex-col">
+                <div className="flex items-center">
+                  <Image
+                    className="mr-2"
+                    src={"/images/player-icon.png"}
+                    width={24}
+                    height={24}
+                    alt="player icon"
+                  />
+                  <div className={styles.count}>300+</div>
+                </div>
+                <div className={styles.name}>Players</div>
+              </div>
             </div>
           </div>
         </div>
